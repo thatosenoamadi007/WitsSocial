c251e6d01d56d9dcc7eee9556318070a5f890f08
thatosenoamadi007 <101407833+thatosenoamadi007@users.noreply.github.com>
2022-09-07 10:53:31 +0000

    Update login.java

--- app/src/main/java/com/example/witssocial_/login.java
+++ app/src/main/java/com/example/witssocial_/login.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    //find all views in login page
     EditText username, password;
     Button logIn;
     FirebaseAuth mAuth;
@@ -20,13 +21,14 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        
+        //initlise all views in activity
         username = findViewById(R.id.loginUsername);
         password = findViewById(R.id.logInPass);
         logIn = findViewById(R.id.logInBtn);
         mAuth = FirebaseAuth.getInstance();
 
-
+        //onClick listener for login button to sign in user with email and password
         logIn.setOnClickListener(view -> mAuth.signInWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnCompleteListener(task -> {
             if(task.isSuccessful()){
                 startActivity(new Intent(login.this, SearchUsers.class));
