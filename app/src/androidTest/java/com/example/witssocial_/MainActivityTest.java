96fa6f41c53376602efcacc5cd5e868e03c00c28
Karabo sepuru <karabolsepuru@gmail.com>
2022-09-28 12:57:35 +0200

    latest

--- app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
@@ -2,18 +2,19 @@ package com.example.witssocial_;
 
 import static android.service.autofill.Validators.not;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static junit.framework.TestCase.assertNotNull;
 
 import android.app.Instrumentation;
-import android.support.test.espresso.Espresso;
+
 import android.support.test.rule.ActivityTestRule;
 import android.widget.EditText;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
