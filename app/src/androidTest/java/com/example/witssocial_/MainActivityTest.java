8822d49f1d787f2865c3d48206ecd9e0deef07b4
thatosenoamadi007 <101407833+thatosenoamadi007@users.noreply.github.com>
2022-10-10 22:47:51 +0200

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
@@ -77,7 +77,7 @@ public class MainActivityTest {
 
 }
 */
-package com.example.witssocial_;
+/*package com.example.witssocial_;
 
 import junit.framework.TestCase;
 
@@ -115,4 +115,87 @@ public class MainActivityTest extends TestCase {
         Post post=new Post("https://firebasestorage.googleapis.com/witssocial/2022/09/06","Top of the morning","karabo@gmail.com","my id");
         assertEquals("karabo@gmail.com",post.getUsername());
     }
+}*/
+package com.example.witssocial_;
+import static android.service.autofill.Validators.not;
+//import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import androidx.test.platform.app.InstrumentationRegistry;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertNotNull;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.EditText;
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    private login mActivity = null;
+    public String wrongDetails = "You are already registered, click the link above to login.";
+    public String correctDetails = "Registered successfully";
+    Instrumentation.ActivityMonitor monitor = InstrumentationRegistry.getInstrumentation().addMonitor(MainActivity.class.getName(),null,false);
+    @Test
+    public void FullNameCheck(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            // use 'activity'.
+            EditText firstname = activity.findViewById(R.id.FullName);
+            assertNotNull(firstname);
+        });
+
+    }
+    @Test
+    public void Emailcheck(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            // use 'activity'.
+            EditText Email = activity.findViewById(R.id.Email);
+            assertNotNull(Email);
+        });
+
+    }
+    @Test
+    public void Password(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            // use 'activity'.
+            EditText Password = activity.findViewById(R.id.Password);
+            assertNotNull(Password);
+        });
+
+    }
+    @Test
+    public void ConPassword(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            // use 'activity'.
+            EditText ConnPassowrd = activity.findViewById(R.id.confirmPassword);
+            assertNotNull(ConnPassowrd);
+        });
+
+    }
+    /* @Before
+     public void setUp() throws Exception {
+        // mActivity = rActivityTestRule.getActivity();
+     }*/
+    @Test
+    public void enterOldDetails(){
+        Espresso.onView(withId(R.id.FullName)).perform(typeText("Neal")).perform(closeSoftKeyboard());
+        Espresso.onView(withId(R.id.Email)).perform(typeText("nealneal@gmail.com")).perform(closeSoftKeyboard());
+        Espresso.onView(withId(R.id.Password)).perform(typeText("123")).perform(closeSoftKeyboard());
+        Espresso.onView(withId(R.id.confirmPassword)).perform(typeText("123")).perform(closeSoftKeyboard());
+        Espresso.onView(withId(R.id.SignUp)).perform(click());
+        //login activity = rActivityTestRule.getActivity();
+        //Espresso.onView(withText(wrongDetails)).inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+    /*@After
+    public void tearDown() throws Exception {
+        mActivity = null ;
+    }*/
 }
