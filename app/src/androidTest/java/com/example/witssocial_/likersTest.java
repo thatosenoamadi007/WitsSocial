9f21ee5d4ebdc5f3eec6131bccebbb5435b34bc2
Karabo sepuru <karabolsepuru@gmail.com>
2022-10-12 19:52:33 +0200

    latest

---
+++ app/src/androidTest/java/com/example/witssocial_/likersTest.java
@@ -0,0 +1,18 @@
+package com.example.witssocial_;
+
+import junit.framework.TestCase;
+
+public class likersTest extends TestCase {
+
+    public void testSetPost() {
+        likers likers=new likers();
+        likers.setLikerID("like");
+        assertEquals("like",likers.getLikerID());
+    }
+
+    public void testGetPost() {
+        likers likers=new likers("like");
+        assertEquals("like",likers.getLikerID());
+    }
+
+}
