5994e4411cb0bc47c8c33e349a87c32b1c00af38
Gerald-Mukumba <mukumbagerald@gmail.com>
2022-10-26 12:36:36 +0200

    Password edit

---
+++ app/src/test/java/com/example/witssocial_/commentTest.java
@@ -0,0 +1,34 @@
+package com.example.witssocial_;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class commentTest {
+
+    @Test
+    public void setComment() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getComment() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void setId() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getId() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+        /*public void addition_isCorrect() {
+
+            assertEquals(4, 2 + 2);
+        }*/
+    }
