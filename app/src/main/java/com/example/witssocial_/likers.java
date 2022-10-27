1e86f713b708bbeab036ba38244356b218b2de23
KaraboSepuru <86748174+KaraboSepuru@users.noreply.github.com>
2022-10-27 14:30:52 +0200

    Update likers.java

--- app/src/main/java/com/example/witssocial_/likers.java
+++ app/src/main/java/com/example/witssocial_/likers.java
@@ -2,18 +2,18 @@ package com.example.witssocial_;
 
 public class likers {
 
-
+    //variable
     String LikerID;
-
+    //constructor without arguments
     public likers(){
 
     }
-
+    //Constructor with arguments
     public likers(String LikerID){
         this.LikerID=LikerID;
 
     }
-
+    //getter and setter methods
     public void setLikerID(String LikerID){
         this.LikerID=LikerID;
     }
