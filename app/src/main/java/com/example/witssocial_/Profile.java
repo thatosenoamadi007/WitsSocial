51455ce928caa16c9f3489c23b4a91b1c3f8900e
Morake17 <77198448+Morake17@users.noreply.github.com>
2022-10-27 12:15:53 +0200

    Update Profile.java

--- app/src/main/java/com/example/witssocial_/Profile.java
+++ app/src/main/java/com/example/witssocial_/Profile.java
@@ -3,7 +3,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Toast;//The 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
