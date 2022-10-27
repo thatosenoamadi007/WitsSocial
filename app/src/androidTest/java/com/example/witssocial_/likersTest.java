1aaf414f1551799174bf438513fa31d72d323d81
Morake17 <77198448+Morake17@users.noreply.github.com>
2022-10-27 11:26:03 +0200

    Update likersTest.java

--- app/src/androidTest/java/com/example/witssocial_/likersTest.java
+++ app/src/androidTest/java/com/example/witssocial_/likersTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class likersTest extends TestCase {
 
-    public void testSetPost() {
+    public void testSetPost() {//Here we test if the liking feature is indeed working properly and liking the image or text
         likers likers=new likers();
         likers.setLikerID("like");
         assertEquals("like",likers.getLikerID());
