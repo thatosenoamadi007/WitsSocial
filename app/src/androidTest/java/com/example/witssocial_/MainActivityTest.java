37fe1f93e956e7e156fe58338ddbe11736efd33a
Morake17 <77198448+Morake17@users.noreply.github.com>
2022-11-11 12:54:20 +0200

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
@@ -4,6 +4,7 @@ import static android.service.autofill.Validators.not;
 import androidx.test.platform.app.InstrumentationRegistry;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static junit.framework.TestCase.assertNotNull;
