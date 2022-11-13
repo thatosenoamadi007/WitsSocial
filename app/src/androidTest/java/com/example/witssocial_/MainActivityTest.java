5f9270100ded22410743fa1f563d7e871bae3d86
Karabo sepuru <karabolsepuru@gmail.com>
2022-11-14 00:47:10 +0200

    latest

--- app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
+++ app/src/androidTest/java/com/example/witssocial_/MainActivityTest.java
@@ -71,7 +71,7 @@ public class MainActivityTest {
     @Test
     public void enterCorrectDetails(){
         Espresso.onView(withId(R.id.FullName)).perform(typeText("Neal Beck")).perform(closeSoftKeyboard());
-        Espresso.onView(withId(R.id.Email)).perform(typeText("nealneal1@gmail.com")).perform(closeSoftKeyboard());
+        Espresso.onView(withId(R.id.Email)).perform(typeText("nealneal2@gmail.com")).perform(closeSoftKeyboard());
         Espresso.onView(withId(R.id.Password)).perform(typeText("1234567")).perform(closeSoftKeyboard());
         Espresso.onView(withId(R.id.confirmPassword)).perform(typeText("1234567")).perform(closeSoftKeyboard());
         Espresso.onView(withId(R.id.SignUp)).perform(click());
