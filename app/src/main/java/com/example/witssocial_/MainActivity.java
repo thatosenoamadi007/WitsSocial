281db48278b4f8f62dcbe568acf094d96bda9f60
Morake17 <77198448+Morake17@users.noreply.github.com>
2022-11-14 11:19:10 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/witssocial_/MainActivity.java
+++ app/src/main/java/com/example/witssocial_/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast; 
 
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
